7da34098c9d3cb5a40c6bee6f0b57eb91b32d9b6
OnurKaraoz <OnurKaraoz@NIRVANASERVER>
2022-04-10 13:55:18 +0300

    Add project files.

---
+++ ArrayDiziler/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayDiziler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //DİZİ TANIMLAMA 1
+            string[] students=new string[4];
+
+            //DİZİ DEĞERVERME
+            students[0] = "Onur";
+            students[1] = "Karaoz";
+            students[2] = "2243";
+
+            foreach (var stList in students)
+            {
+                Console.WriteLine(stList);
+            }
+
+            //DİZİ TANIMLAMA VE DEĞER VERME
+            int[] numbers = new int[4] {1453,1234,2345,0};
+
+            foreach (var stList in numbers)
+            {
+                Console.WriteLine(stList);
+            }
+
+            ////////////////////////////
+            //ÇOK BOYUTLU DİZİ TANIMLAMA
+            ////////////////////////////
+            string[,] regions=new string[5,3]
+            {
+                {"İstanbul","Edirne","Balıkesir"},
+                {"Ankara","Konya","Isparta"},
+                {"Antalya","Adana","Mersin"},
+                {"Rize","Trabzon","Samsun"},
+                {"İzmir","Muğla","Manisa"},        
+            };
+            //7 Satır 
+            //3 Sütun
+            for (int i = 0; i <= regions.GetUpperBound(0); i++) //Satırları Gezdim
+            {
+                for (int j = 0; j <= regions.GetUpperBound(1); j++) 
+                {
+                    Console.WriteLine(regions[i, j]);
+                }
+                Console.WriteLine("---------------");
+            }
+
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AsalSayiKontrol/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsalSayiKontrol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kendisi ve 1 Haricinde bölünemeyen sayıdır..
+            if (isPrivateNumber(6))
+            {
+                Console.WriteLine("This is a private  number ");
+            }
+            else
+            {
+                Console.WriteLine("Not Private number");
+            }
+            Console.ReadLine();
+        }
+        private static bool isPrivateNumber(int number)
+        {
+            bool isPrivate = true;
+            for (int i = 2; i < number-1; i++)
+            {
+                if (number%i==0)
+                {
+                    isPrivate = false;
+                    i = number;
+
+                }
+            }
+            return isPrivate;
+        }
+    }
+}
