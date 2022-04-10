64be09bf403a36fc5eb866a2304fe351e4514510
OnurKaraoz <OnurKaraoz@NIRVANASERVER>
2022-04-11 02:40:47 +0300

    change name

---
+++ Classes_Abstract/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Abstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Database database = new SqlServer();
+            database.Delete();
+            database.Add();
+
+            Console.ReadLine();
+        }
+        abstract class Database
+        {
+            public void Add()
+            {
+                Console.WriteLine("Added");
+            }
+            public abstract void Delete();
+        }
+        class SqlServer : Database
+        {
+            public override void Delete()
+            {
+                Console.WriteLine("Deleted Sql");
+            }
+        }
+        class Oracle : Database
+        {
+            public override void Delete()
+            {
+                Console.WriteLine("Deleted Oracle");
+            }
+        }
+    }
+}
