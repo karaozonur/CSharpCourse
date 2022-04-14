23b13bbd5fb9a7474ba9cf9f39cc46f64ff771a5
Onur Karaöz <karaoz.onr@gmail.com>
2022-04-15 02:56:43 +0300

    // Mulakat Sorulari

---
+++ 111.4_DegerveReferansTipler/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _111._4_DegerveReferansTipler
+{
+    internal class Program
+    {
+        // Value -> Değer
+        // Referances -> Referans
+        static void Main(string[] args)
+        {
+        }
+        // STACK (YIĞIN) --> DEĞER TİP
+        //Yani SAYISAL olanlar Enum ve Boolean Mantıksal ifadeler DEĞER TİPTİR. int Double , Enum, Değer tiptir
+        //DEĞER TİP STACK TE TANIMLANIR VE STACKTE DEĞER ATILIR
+        //SAYI1 = SAYI2 SAYI BİRİN DEĞERİ SAYI2 NİN DEĞERİ
+
+        // HEAP (YIĞIN) -- > REFERANS TİP 
+        // Class,  Array(Dizi), interface, Abstracclass
+        // Array int bool (yani mantıksal ve sayısalda olsa) diziler REFERANS TİPTİR.
+        // REFERANS TİPLER NEW LENENLERDİR
+        // ARRAYDE STRTİNG[] SEHİRLER -> STACKTA TANIMLANIR =NEW STRİNG[]{"A,B,C"} HEAP İÇİNDE NUMARALANDIRILIR.
+        
+        
+        //*** String referans tiptir ama çalışma mantığı referans tip gibidir.
+    }
+}
---
+++ 111.5_Interface_Nedir_Ne_ise_Yarar/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _111._5_Interface_Nedir_Ne_ise_Yarar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
